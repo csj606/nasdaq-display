0f60b2cafd125dd49bcbb138cf0b6cb8bc0563f3
csj606 <139289685+csj606@users.noreply.github.com>
2025-05-14 21:14:45 -0400

    Added sanitization
    Added a function that verified the symbol entered was a valid NASDAQ stock.

--- NasdaqDisplay/src/relay.tsx
+++ NasdaqDisplay/src/relay.tsx
@@ -1,12 +1,34 @@
 import {restClient} from '@polygon.io/client-js';
 const polyon = restClient("");
 
+const nasdaqStockTickers: string[] = ["MSFT", "AAPL", "NVDA", "AMZN", "AVGO", "META", "NFLX",
+    "TSLA", "COST", "GOOG", "TMUS", "PLTR", "CSCO", "LIN", "ISRG", "INTU", "PEP", "AMD", "ADBE", 
+    "TXN", "BKNG", "QCOM", "AMGN", "HON", "AMAT", "CMCSA", "GILD", "PANW", "MELI", "ADP", 
+    "VRTX", "ADI", "APP", "LRCX", "MU", "KLAC", "CRWD", "SBUX", "MSTR", "INTC", "CEG", "CTAS",
+    "CDNS", "MDLZ", "FTNT", "SNPS", "PDD", "ORLY", "DASH", "MAR", "PYPL", "ASML", "ADSK", "REGN",
+    "ROP", "CPRT", "MNST", "ABNB", "CSX", "CHTR", "WDAY", "MRVL", "PAYX", "AEP", "AXON", "NXPI",
+    "PCAR", "ROST", "FAST", "KDP", "EXC", "VRSK", "IDXX", "FANG", "CTSH", "CCEP", "AZN", "TTWO",
+    "EA", "XEL", "ODFL", "BKR", "ZS", "TEAM", "DDOG", "TTD", "LULU", "GEHC", "KHC", "DXCM", "MCHP",
+    "CSGP", "ANSS", "CDW", "WBD", "GFS", "ON", "BIIB", "ARM", "MDB"
+];
+
 type Stock =  {
     symbol: string;
     recent_price: number;
 };
 
+function verifyTicker(ticker: string): boolean{
+    if(ticker in nasdaqStockTickers){
+        return true
+    }else{
+        return false
+    }
+}
+
 async function retrieveLastestPrice(ticker: string): Promise<Stock>{
+    if(!verifyTicker(ticker)){
+        return {symbol: "Invalid Stock Symbol", recent_price: -1}
+    }
     const data = await polyon.stocks.lastQuote(ticker);
     try{
         if(data.results !== null){
