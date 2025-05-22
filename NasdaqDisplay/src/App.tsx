05786c73ef036bee5df8554f7838bc9aaabe5f4d
csj606 <139289685+csj606@users.noreply.github.com>
2025-05-21 22:51:17 -0400

    Stock Card Added
    Created component for a stock display in a table. Added function for a company name lookup.

--- NasdaqDisplay/src/App.tsx
+++ NasdaqDisplay/src/App.tsx
@@ -2,20 +2,20 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import './relay.tsx'
+import { retrieveCompanyName, retrieveLastestPrice } from './relay.tsx'
+ 
+function StockCard(ticker: string) {
+  const price = retrieveLastestPrice(ticker)
+  const fullName = retrieveCompanyName(ticker)
+  
+}
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
--- NasdaqDisplay/src/relay.tsx
+++ NasdaqDisplay/src/relay.tsx
@@ -25,7 +25,7 @@ function verifyTicker(ticker: string): boolean{
     }
 }
 
-async function retrieveLastestPrice(ticker: string): Promise<Stock>{
+export async function retrieveLastestPrice(ticker: string): Promise<Stock>{
     if(!verifyTicker(ticker)){
         return {symbol: "Invalid Stock Symbol", recent_price: -1}
     }
@@ -45,5 +45,214 @@ async function retrieveLastestPrice(ticker: string): Promise<Stock>{
             recent_price: -1
         }
     }
+}
+
+export function retrieveCompanyName(ticker: string): string{
+    if(!verifyTicker(ticker)){
+        return "Invaid ticker"
+    }
+    switch(ticker){
+        case "MSFT":
+            return "Microsoft"
+        case "AAPL":
+            return "Apple"
+        case "NVDA":
+            return "Nvidia"
+        case "AMZN":
+            return "Amazon"
+        case "AVGO":
+            return "Broadcom"
+        case "META":
+            return "Meta"
+        case "NFLX":
+            return "Netflix"
+        case "TSLA":
+            return "Tesla"
+        case "COST":
+            return "Costco"
+        case "GOOG":
+            return "Google"
+        case "TMUS":
+            return "T-Mobile US"
+        case "PLTR":
+            return "Palantir"
+        case "CSCO":
+            return "Cisco"
+        case "LIN":
+            return "Linde"
+        case "ISRG":
+            return "Intuitive Surgical"
+        case "INTU":
+            return "Intuit"
+        case "PEP":
+            return "PepsiCo"
+        case "AMD":
+            return "Advanced Micro Devices"
+        case "ADBE":
+            return "Adobe"
+        case "VRTX":
+            return "Vertex Pharmaceuticals"
+        case "TXN":
+            return "Texas Instruments"
+        case "BKNG":
+            return "Booking Holdings"
+        case "QCOM":
+            return "Qualcomm"
+        case "AMGN":
+            return "Amgen"
+        case "HON":
+            return "Honeywell"
+        case "AMAT":
+            return "Applied Materials"
+        case "CMCSA":
+            return "Comcast"
+        case "GILD":
+            return "Gilead Science"
+        case "PANW":
+            return "Palo Alto Networks"
+        case "MELI":
+            return "MercadoLibre"
+        case "ADP":
+            return "Automatic Data Processing"
+        case "ADI":
+            return "Analog Devices"
+        case "APP":
+            return "Applovin"
+        case "LRCX":
+            return "Lam Research"
+        case "MU":
+            return "Micron"
+        case "KLAC":
+            return "KLA Corporation"
+        case "CRWD":
+            return "CrowdStrike Holdings"
+        case "SBUX":
+            return "Starbucks"
+        case "MSTR":
+            return "MicroStrategy"
+        case "INTC":
+            return "Intel"
+        case "CEG":
+            return "Constellation Energy"
+        case "CTAS":
+            return "Cintas"
+        case "CDNS":
+            return "Cadence Design Systems"
+        case "MDLZ":
+            return "Mondelez International"
+        case "FTNT":
+            return "Fortinet"
+        case "SNPS":
+            return "Synopsys"
+        case "PDD": 
+            return "PDD Holdings"
+        case "ORLY":
+            return "O'Reilly Automative"
+        case "DASH":
+            return "DoorDash"
+        case "MAR":
+            return "Marriott International"
+        case "PYPL":
+            return "PayPal"
+        case "ASML":
+            return "ASML Holding"
+        case "ADSK":
+            return "AutoDesk"
+        case"REGN":
+            return "Regeneron Pharmaceuticals"
+        case "ROP":
+            return "Roper Technologies"
+        case "CPRT":
+            return "Copart Inc"
+        case "MNST":
+            return "Monster Beverage"
+        case "ABNB":
+            return "Airbnb"
+        case "CSX":
+            return "CSX Corporation"
+        case "CHTR":
+            return "Charter Communcations"
+        case "WDAY":
+            return "Workday"
+        case "MRVL":
+            return "Marvell Technology"
+        case "PAYX":
+            return "Paychex Inc."
+        case "AEP":
+            return "American Electric Power"
+        case "AXON":
+            return "Axon Enterprise"
+        case "NXPI":
+            return "NXP Semiconductors"
+        case "PCAR":
+            return "PACCAR Inc"
+        case "ROST":
+            return "Ross Stores"
+        case "FAST":
+            return "Fastenal"
+        case "KDP":
+            return "Keurig Dr. Pepper"
+        case "EXC":
+            return "Exelon"
+        case "VRSK":
+            return "Verisk Analytics"
+        case "IDXX":
+            return "IDEXX Laboratories"
+        case "FANG":
+            return "Diamondback Energy"
+        case "CTSH":
+            return "Cognizant Technologies"
+        case "CCEP":
+            return "Coca-Cola Europacific Partners"
+        case "AZN":
+            return "AstraZeneca"
+        case "TTWO":
+            return "Take Two Interactive Solutions"
+        case "EA":
+            return "Electronic Arts"
+        case "XEL":
+            return "Xcel Energy"
+        case "ODFL":
+            return "Old Dominion Freight Line"
+        case "BKR":
+            return "Baker Hughes"
+        case "ZS":
+            return "Zscalar"
+        case "TEAM":
+            return "Atlassian"
+        case "DDOG":
+            return "Datadog"
+        case "TTD":
+            return "The Trade Desk"
+        case "LULU":
+            return "Luluemon Atheletica"
+        case "GEHC":
+            return "GE HealthCare Technologies"
+        case "KHC":
+            return "Kraft Heinz"
+        case "DXCM":
+            return "DexCom"
+        case "MCHP":
+            return "Microchip Technologies"
+        case "CSGP":
+            return "CoStar"
+        case "ANSS":
+            return "Ansys"
+        case "CDW":
+            return "CDW"
+        case "WBD":
+            return "Warner Brothers"
+        case "GFS":
+            return "GlobalFoundries"
+        case "ON":
+            return "ON Semiconductor"
+        case "BIIB":
+            return "Biogen"
+        case "ARM":
+            return "Arm Holdings"
+        case "MDB":
+            return "MongoDB"
+    }
+    return "Invalid Ticker"
 }
 
